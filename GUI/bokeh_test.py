9fe6606d263363cd1b147accf4478f3649bc63a0
caimeng <862786917@qq.com>
2016-11-17 17:42:56 +0800

    20161117

--- GUI/bokeh_test.py
+++ GUI/bokeh_test.py
@@ -7,30 +7,36 @@ from datetime import datetime,timedelta
 import pandas
 from support.talibs import getIndicator
 
-def candle(up,down):
-    output_file('plot.html')
 
-    plot=figure(title='candle',height=300, x_axis_type="datetime",width=1000)
-    plot.add_tools(HoverTool(),BoxSelectTool())
-    plot.quad(left=up.left,right=up.right,top=up.closeMid,bottom=up.openMid)
-    plot.quad(left=down.left,right=down.right,top=down.openMid,bottom=down.closeMid,color='#AA1111')
-    plot.segment(x0=up.index,y0=up.lowMid,x1=up.index,y1=up.highMid)
-    plot.segment(x0=down.index,y0=down.lowMid,x1=down.index,y1=down.highMid,color='#AA1111')
-    return plot
 
 
 if __name__ == '__main__':
     client=connectDB('FinanceData')
     data=client.Data['HSI'].find(
         {'tradeDate':{'$gte':datetime(2015,1,1),'$lte':datetime(2016,11,1)}},
-        projection=['tradeDate','closeIndex']
+        projection=['tradeDate','closeIndex','highestIndex','lowestIndex','openIndex']
     ).toDataFrame()
     from extra_plot import candle,histogram
     output_file('plot.html')
-    # plot=candle(data,c_width=timedelta(hours=10),width=1000,x_axis_type="datetime",title='candle')
-    MACD=getIndicator('MACD',data['tradeDate'],data['closeIndex'])
-    MACD=MACD[[2,'time']]
-    MACD.columns=['value','datetime']
-    plot=histogram(MACD,c_width=timedelta(hours=10),x_axis_type='datetime',width=1000)
-    show(plot)
+    data['datetime']=data.pop('tradeDate')
+    plot=candle(data,c_width=timedelta(hours=10),width=1500,x_axis_type="datetime",title='candle',height=350,
+                trans={'high':'highestIndex','low':'lowestIndex','open':'openIndex','close':'closeIndex'}
+                )
+
+    # for i in data.index[1:]:
+    #     data
+
+    data['range']=0
+    for i in data.index[1:]:
+        data.set_value(i,'range',
+                       data.get_value(i,'closeIndex')-data.get_value(i-1,'closeIndex'))
+    data['range']=data['range']/data['closeIndex']
+
+    plot2=histogram(data[['datetime','range']],c_width=timedelta(hours=10),value='range',
+                    width=1500,height=350,x_axis_type="datetime")
+
+    plot2.x_range=plot.x_range
+
+    grid=gridplot([[plot],[plot2]])
+    show(grid)
 
--- GUI/extra_plot.py
+++ GUI/extra_plot.py
@@ -2,6 +2,15 @@ from bokeh.plotting import figure
 from datetime import timedelta
 
 def candle(df,c_width,upColor='#AA1111',downColor='#1188AA',**kwargs):
+    '''
+
+    :param df:
+    :param c_width:
+    :param upColor:
+    :param downColor:
+    :param kwargs:
+    :return:
+    '''
     trans=kwargs.pop(
         'trans',
         {'high':'highMid','low':'lowMid','open':'openMid','close':'closeMid'}
@@ -35,7 +44,7 @@ def histogram(df,c_width,value='value',dt='datetime',color='#115599',**kwargs):
     down=df[df[value]<0]
 
     plot=figure(**kwargs)
-    plot.quad(left=up.left,right=up.right,top=up.value,bottom=0,color=color)
-    plot.quad(left=down.left,right=down.right,top=0,bottom=down.value,color=color)
+    plot.quad(left=up.left,right=up.right,top=up[value],bottom=0,color=color)
+    plot.quad(left=down.left,right=down.right,top=0,bottom=down[value],color=color)
 
     return plot
